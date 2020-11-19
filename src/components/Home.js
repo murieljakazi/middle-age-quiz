b8444aeb10c34c7dc3d68f1f2f2df287d9db03fe
Muriel Jakazi <murielnjakazi@gmail.com>
2020-11-19 17:26:26 +0100

    initialised repo with Home component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    state = {
+        questionBank : []
+    }
+    getQuestions = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+                <h1>Quiz Up - Middle Ages</h1>
+                <div></div>
+            </div>
+        )
+    }
+
+
+}
+
+export default Home; 
