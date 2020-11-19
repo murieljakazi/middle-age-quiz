776967a4c31a731660a07953f8d80017a9e6ce62
Christopher Saury <saurychristopher@gmail.com>
2020-11-19 22:09:08 +0100

    I've added the quiz button with a feature to hide 2 answer out of one selected

--- src/components/MiddleAge.js
+++ src/components/MiddleAge.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { questionBank } from './QuestionBank/QuestionBank';
+import React, { useState, useEffect } from "react";
+import { questionBank } from "./QuestionBank/QuestionBank";
+import QuestionBox from "./QuestionBox/QuestionBox";
 
 const MiddleAge = () => {
     const [questions, setQuestions] = useState([]);
-    
+
     useEffect(()=> {
         const displayQuestions = questionBank
         setQuestions(displayQuestions)
     }, [])
         return(
             <div>
-                {questions.map(question => <h4>{question.question}</h4> )}
+                {questions.map(question => <QuestionBox key={question.questionId}question={question.question} 
+                options={question.answers}/> )}
                 <p>Middle Age</p>
             </div>
         )
---
+++ src/components/QuestionBox/QuestionBox.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const QuestionBox = ({question,options}) => {
+    const [answer, setAnswer] = useState(options)
+    return(
+        <div className="questionBox">
+            <div className="questionQuiz">{question}</div>
+            {answer.map((text, index) =>(
+                <button key={index} className="answerBtn" onClick={()=> {
+                    setAnswer([text])
+                }}>
+                    {text}</button>
+            ))}
+        </div>
+    )
+}
+
+export default QuestionBox
